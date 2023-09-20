0d70314094d6f549ded8b90aba578c8db141132d
Muhammadsuleman31 <98824481+Muhammadsuleman31@users.noreply.github.com>
2023-09-20 17:39:34 +0500

    final

--- client/src/SignupForm/SignupForm.jsx
+++ client/src/SignupForm/SignupForm.jsx
@@ -123,7 +123,7 @@ async function done(e){
           Password:
           <div style={{ display: 'flex', alignItems: 'center' }}>
            <input type={showPassword ? 'text' : 'password'} name="password" required onChange={changeHandler} value={userData.password}   className={passwordr}  /> 
-           {!validpassword && (<Tooltip title="Password must contain at least 8 characters with uppercase, lowercase, digits, and special characters" placement="right" arrow> <i className="fas fa-exclamation-circle" style={{ color: '#FF7F7F', fontSize: '24px' }}></i></Tooltip>)}
+           {!validpassword && (<Tooltip title="Password must contain at least 8 characters with uppercase, lowercase, digits, and special characters." placement="right" arrow> <i className="fas fa-exclamation-circle" style={{ color: '#FF7F7F', fontSize: '24px' }}></i></Tooltip>)}
           </div> 
            <div className={`please ${!validpassword ? "good" : ""}`}>Password must contain at least 8 characters with uppercase, lowercase, digits, and special characters</div>
           <input type="checkbox" onChange={toggle}/><span className="pass">show password</span>
